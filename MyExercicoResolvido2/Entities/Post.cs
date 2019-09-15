9d2b0edb2ad03df4ce5713c70781e748762c6987
Gabriel <gabriel.pds2001@gmail.com>
2019-09-15 15:09:31 -0300

    Post Unico

---
+++ MyExercicoResolvido2/Entities/Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyExercicoResolvido2.Entities
+{
+    class Post
+    {
+        public string Title { get; set; }
+        public DateTime Moment { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+        public Post(string title) : this()
+        {
+            Title = title;
+        }
+        public Post(string title, string content) : this(title)
+        {
+            Content = content;
+        }
+
+        public override string ToString()
+        {
+            return Title.ToUpper() + "\n" + Likes + " Likes - " + Moment + "\n" + Content;          
+        }
+        public void ShowComments()
+        {
+            foreach (Comment item in Comments)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
---
+++ MyExercicoResolvido2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using MyExercicoResolvido2.Entities;
+
+namespace MyExercicoResolvido2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post p1 = new Post();
+
+            Console.Write("TITLE: ");
+            string title = Console.ReadLine();
+            Console.WriteLine("Content: ");
+            string content = Console.ReadLine();
+
+            Console.Write("Show Comments? [s/n]   ");
+            char comment = char.Parse(Console.ReadLine());
+            bool boolComent = (comment == 's') ? true : false;
+
+            Console.Write("Show Likes?    [s/n]   ");
+            char likes = char.Parse(Console.ReadLine());
+            bool boolLike = (likes == 's') ? true : false;
+
+            Console.WriteLine();
+            Console.WriteLine("Submit Post? [s/n] ");
+            char confirm = char.Parse(Console.ReadLine());
+            bool boolConfirm = (confirm == 's') ? true : false;
+
+            if (boolConfirm)
+            {
+                p1 = new Post(title, content);
+            }
+
+            if (boolComent)
+            {
+                Console.Write("How much comments there: ");
+                int n = int.Parse(Console.ReadLine());
+                for(int i = 0; i< n; i++)
+                {
+                    Console.Write("Comment #{0}: ",i+1);
+                    Comment text = new Comment { Text = Console.ReadLine() };
+                    p1.Comments.Add(text);
+                }
+            }
+            if (boolLike)
+            {
+                Console.Write("How many Likes recived this post: ");
+                int postLikes = int.Parse(Console.ReadLine());
+                p1.Likes = postLikes;
+            }
+
+           
+
+
+            Console.WriteLine();
+            Console.WriteLine(p1);
+            Console.WriteLine("\nComents: \n");
+            p1.ShowComments();
+        }
+    }
+}
